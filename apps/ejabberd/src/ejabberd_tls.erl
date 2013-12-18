b488a9e307d8cdd4e22be3af262513a68ec14cb1
huangxiangdan <xd_huang1986@163.com>
2013-12-18 14:31:24 +0800

    first commit to minimum mongooseIM
    1. fix 1.2.2 release bug
    2. remove many unuse module

--- apps/ejabberd/src/ejabberd_tls.erl
+++ apps/ejabberd/src/ejabberd_tls.erl
@@ -1,5 +1,5 @@
 %%%----------------------------------------------------------------------
-%%% File    : tls.erl
+%%% File    : ejabberd_tls.erl
 %%% Author  : Alexey Shchepin <alexey@process-one.net>
 %%% Purpose : Interface to openssl
 %%% Created : 24 Jul 2004 by Alexey Shchepin <alexey@process-one.net>
@@ -24,31 +24,21 @@
 %%%
 %%%----------------------------------------------------------------------
 
--module(tls).
+-module(ejabberd_tls).
 -author('alexey@process-one.net').
 
--behaviour(gen_server).
-
--export([start/0, start_link/0,
-	 tcp_to_tls/2, tls_to_tcp/1,
-	 send/2,
-	 recv/2, recv/3, recv_data/2,
-	 setopts/2,
-	 sockname/1, peername/1,
-	 controlling_process/2,
-	 close/1,
-	 get_peer_certificate/1,
-	 get_verify_result/1,
-	 get_cert_verify_string/2,
-	 test/0]).
-
-%% Internal exports, call-back functions.
--export([init/1,
-	 handle_call/3,
-	 handle_cast/2,
-	 handle_info/2,
-	 code_change/3,
-	 terminate/2]).
+-export([start/0,
+   tcp_to_tls/2, tls_to_tcp/1,
+   send/2,
+   recv/2, recv/3, recv_data/2,
+   setopts/2,
+   sockname/1, peername/1,
+   controlling_process/2,
+   close/1,
+   get_peer_certificate/1,
+   get_verify_result/1,
+   get_cert_verify_string/2,
+   test/0]).
 
 -include("ejabberd.hrl").
 
@@ -74,84 +64,38 @@
 -record(tlssock, {tcpsock, tlsport}).
 
 start() ->
-    gen_server:start({local, ?MODULE}, ?MODULE, [], []).
-
-start_link() ->
-    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
-
-init([]) ->
     case erl_ddll:load_driver(ejabberd:get_so_path(), tls_drv) of
-	ok -> ok;
-	{error, already_loaded} -> ok
-    end,
-    Port = open_port({spawn, tls_drv}, [binary]),
-    Res = port_control(Port, ?SET_CERTIFICATE_FILE_ACCEPT, "./ssl.pem" ++ [0]),
-    case Res of
-	<<0>> ->
-	    %ets:new(iconv_table, [set, public, named_table]),
-	    %ets:insert(iconv_table, {port, Port}),
-	    {ok, Port};
-	<<1, Error/binary>> ->
-	    {error, binary_to_list(Error)}
+  ok -> ok;
+  {error, permanent} -> ok;
+  {error, already_loaded} -> ok
     end.
-
-
-%%% --------------------------------------------------------
-%%% The call-back functions.
-%%% --------------------------------------------------------
-
-handle_call(_, _, State) ->
-    {noreply, State}.
-
-handle_cast(_, State) ->
-    {noreply, State}.
-
-handle_info({'EXIT', Port, Reason}, Port) ->
-    {stop, {port_died, Reason}, Port};
-
-handle_info({'EXIT', _Pid, _Reason}, Port) ->
-    {noreply, Port};
-
-handle_info(_, State) ->
-    {noreply, State}.
-
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
-
-terminate(_Reason, Port) ->
-    Port ! {self, close},
-    ok.
 
 
 tcp_to_tls(TCPSocket, Options) ->
     case lists:keysearch(certfile, 1, Options) of
-	{value, {certfile, CertFile}} ->
-	    case erl_ddll:load_driver(ejabberd:get_so_path(), tls_drv) of
-		ok -> ok;
-		{error, already_loaded} -> ok
-	    end,
-	    Port = open_port({spawn, tls_drv}, [binary]),
-	    Flags =
-		case lists:member(verify_none, Options) of
-		    true ->
-			?VERIFY_NONE;
-		    false ->
-			0
-		end,
-	    Command = case lists:member(connect, Options) of
-			  true ->
-			      ?SET_CERTIFICATE_FILE_CONNECT;
-			  false ->
-			      ?SET_CERTIFICATE_FILE_ACCEPT
-		      end,
-	    case port_control(Port, Command bor Flags, CertFile ++ [0]) of
-		<<0>> ->
-		    {ok, #tlssock{tcpsock = TCPSocket, tlsport = Port}};
-		<<1, Error/binary>> ->
-		    {error, binary_to_list(Error)}
-	    end;
-	false ->
-	    {error, no_certfile}
+  {value, {certfile, CertFile}} ->
+      Port = open_port({spawn, tls_drv}, [binary]),
+      Flags =
+    case lists:member(verify_none, Options) of
+        true ->
+      ?VERIFY_NONE;
+        false ->
+      0
+    end,
+      Command = case lists:member(connect, Options) of
+        true ->
+            ?SET_CERTIFICATE_FILE_CONNECT;
+        false ->
+            ?SET_CERTIFICATE_FILE_ACCEPT
+          end,
+      case port_control(Port, Command bor Flags, CertFile ++ [0]) of
+    <<0>> ->
+        {ok, #tlssock{tcpsock = TCPSocket, tlsport = Port}};
+    <<1, Error/binary>> ->
+        {error, binary_to_list(Error)}
+      end;
+  false ->
+      {error, no_certfile}
     end.
     
 tls_to_tcp(#tlssock{tcpsock = TCPSocket, tlsport = Port}) ->
@@ -165,63 +109,63 @@ recv(#tlssock{tcpsock = TCPSocket} = TLSSock,
     %% The Length argument cannot be used for gen_tcp:recv/3, because the
     %% compressed size does not equal the desired uncompressed one.
     case gen_tcp:recv(TCPSocket, 0, Timeout) of
-	{ok, Packet} ->
-	    recv_data(TLSSock, Packet);
-	{error, _Reason} = Error ->
-	    Error
+  {ok, Packet} ->
+      recv_data(TLSSock, Packet);
+  {error, _Reason} = Error ->
+      Error
     end.
 
 recv_data(TLSSock, Packet) ->
     case catch recv_data1(TLSSock, Packet) of
-	{'EXIT', Reason} ->
-	    {error, Reason};
-	Res ->
-	    Res
+  {'EXIT', Reason} ->
+      {error, Reason};
+  Res ->
+      Res
     end.
 
 recv_data1(#tlssock{tcpsock = TCPSocket, tlsport = Port}, Packet) ->
     case port_control(Port, ?SET_ENCRYPTED_INPUT, Packet) of
-	<<0>> ->
-	    case port_control(Port, ?GET_DECRYPTED_INPUT, []) of
-		<<0, In/binary>> ->
-		    case port_control(Port, ?GET_ENCRYPTED_OUTPUT, []) of
-			<<0, Out/binary>> ->
-			    case gen_tcp:send(TCPSocket, Out) of
-				ok ->
-				    %?PRINT("IN: ~p~n", [{TCPSocket, binary_to_list(In)}]),
-				    {ok, In};
-				Error ->
-				    Error
-			    end;
-			<<1, Error/binary>> ->
-			    {error, binary_to_list(Error)}
-		    end;
-		<<1, Error/binary>> ->
-		    {error, binary_to_list(Error)}
-	    end;
-	<<1, Error/binary>> ->
-	    {error, binary_to_list(Error)}
+  <<0>> ->
+      case port_control(Port, ?GET_DECRYPTED_INPUT, []) of
+    <<0, In/binary>> ->
+        case port_control(Port, ?GET_ENCRYPTED_OUTPUT, []) of
+      <<0, Out/binary>> ->
+          case gen_tcp:send(TCPSocket, Out) of
+        ok ->
+            %?PRINT("IN: ~p~n", [{TCPSocket, binary_to_list(In)}]),
+            {ok, In};
+        Error ->
+            Error
+          end;
+      <<1, Error/binary>> ->
+          {error, binary_to_list(Error)}
+        end;
+    <<1, Error/binary>> ->
+        {error, binary_to_list(Error)}
+      end;
+  <<1, Error/binary>> ->
+      {error, binary_to_list(Error)}
     end.
 
 send(#tlssock{tcpsock = TCPSocket, tlsport = Port} = TLSSock, Packet) ->
     case port_control(Port, ?SET_DECRYPTED_OUTPUT, Packet) of
-	<<0>> ->
-	    %?PRINT("OUT: ~p~n", [{TCPSocket, lists:flatten(Packet)}]),
-	    case port_control(Port, ?GET_ENCRYPTED_OUTPUT, []) of
-		<<0, Out/binary>> ->
-		    gen_tcp:send(TCPSocket, Out);
-		<<1, Error/binary>> ->
-		    {error, binary_to_list(Error)}
-	    end;
-	<<1, Error/binary>> ->
-	    {error, binary_to_list(Error)};
-	<<2>> -> % Dirty hack
-	    receive
-		{timeout, _Timer, _} ->
-		    {error, timeout}
-	    after 100 ->
-		    send(TLSSock, Packet)
-	    end
+  <<0>> ->
+      %?PRINT("OUT: ~p~n", [{TCPSocket, lists:flatten(Packet)}]),
+      case port_control(Port, ?GET_ENCRYPTED_OUTPUT, []) of
+    <<0, Out/binary>> ->
+        gen_tcp:send(TCPSocket, Out);
+    <<1, Error/binary>> ->
+        {error, binary_to_list(Error)}
+      end;
+  <<1, Error/binary>> ->
+      {error, binary_to_list(Error)};
+  <<2>> -> % Dirty hack
+      receive
+    {timeout, _Timer, _} ->
+        {error, timeout}
+      after 100 ->
+        send(TLSSock, Packet)
+      end
     end.
 
 
@@ -243,18 +187,18 @@ close(#tlssock{tcpsock = TCPSocket, tlsport = Port}) ->
 
 get_peer_certificate(#tlssock{tlsport = Port}) ->
     case port_control(Port, ?GET_PEER_CERTIFICATE, []) of
-	<<0, BCert/binary>> ->
-	    {CertMod, CertFun, CertSecondArg} = ?CERT_DECODE,
-	    case catch apply(CertMod, CertFun, [BCert, CertSecondArg]) of
-		{ok, Cert} -> %% returned by R13 and older
-		    {ok, Cert};
-		{'Certificate', _, _, _} = Cert ->
-		    {ok, Cert};
-		_ ->
-		    error
-	    end;
-	<<1>> ->
-	    error
+  <<0, BCert/binary>> ->
+      {CertMod, CertFun, CertSecondArg} = ?CERT_DECODE,
+      case catch apply(CertMod, CertFun, [BCert, CertSecondArg]) of
+    {ok, Cert} -> %% returned by R13 and older
+        {ok, Cert};
+    {'Certificate', _, _, _} = Cert ->
+        {ok, Cert};
+    _ ->
+        error
+      end;
+  <<1>> ->
+      error
     end.
 
 get_verify_result(#tlssock{tlsport = Port}) ->
@@ -263,20 +207,17 @@ get_verify_result(#tlssock{tlsport = Port}) ->
 
 
 test() ->
-    case erl_ddll:load_driver(ejabberd:get_so_path(), tls_drv) of
-	ok -> ok;
-	{error, already_loaded} -> ok
-    end,
+    start(),
     Port = open_port({spawn, tls_drv}, [binary]),
     ?PRINT("open_port: ~p~n", [Port]),
     PCRes = port_control(Port, ?SET_CERTIFICATE_FILE_ACCEPT,
-			 "./ssl.pem" ++ [0]),
+       "./ssl.pem" ++ [0]),
     ?PRINT("port_control: ~p~n", [PCRes]),
     {ok, ListenSocket} = gen_tcp:listen(1234, [binary,
-					       {packet, 0}, 
-					       {active, true},
-					       {reuseaddr, true},
-					       {nodelay, true}]),
+                 {packet, 0}, 
+                 {active, true},
+                 {reuseaddr, true},
+                 {nodelay, true}]),
     ?PRINT("listen: ~p~n", [ListenSocket]),
     {ok, Socket} = gen_tcp:accept(ListenSocket),
     ?PRINT("accept: ~p~n", [Socket]),
@@ -285,34 +226,34 @@ test() ->
 
 loop(Port, Socket) ->
     receive
-	{tcp, Socket, Data} ->
-	    %?PRINT("read: ~p~n", [Data]),
-	    Res = port_control(Port, ?SET_ENCRYPTED_INPUT, Data),
-	    ?PRINT("SET_ENCRYPTED_INPUT: ~p~n", [Res]),
+  {tcp, Socket, Data} ->
+      %?PRINT("read: ~p~n", [Data]),
+      Res = port_control(Port, ?SET_ENCRYPTED_INPUT, Data),
+      ?PRINT("SET_ENCRYPTED_INPUT: ~p~n", [Res]),
 
-	    DIRes = port_control(Port, ?GET_DECRYPTED_INPUT, Data),
-	    ?PRINT("GET_DECRYPTED_INPUT: ~p~n", [DIRes]),
-	    case DIRes of
-		<<0, In/binary>> ->
-		    ?PRINT("input: ~s~n", [binary_to_list(In)]);
-		<<1, DIError/binary>> ->
-		    ?PRINT("GET_DECRYPTED_INPUT error: ~p~n", [binary_to_list(DIError)])
-	    end,
+      DIRes = port_control(Port, ?GET_DECRYPTED_INPUT, Data),
+      ?PRINT("GET_DECRYPTED_INPUT: ~p~n", [DIRes]),
+      case DIRes of
+    <<0, In/binary>> ->
+        ?PRINT("input: ~s~n", [binary_to_list(In)]);
+    <<1, DIError/binary>> ->
+        ?PRINT("GET_DECRYPTED_INPUT error: ~p~n", [binary_to_list(DIError)])
+      end,
 
-	    EORes = port_control(Port, ?GET_ENCRYPTED_OUTPUT, Data),
-	    ?PRINT("GET_ENCRYPTED_OUTPUT: ~p~n", [EORes]),
-	    case EORes of
-		<<0, Out/binary>> ->
-		    gen_tcp:send(Socket, Out);
-		<<1, EOError/binary>> ->
-		    ?PRINT("GET_ENCRYPTED_OUTPUT error: ~p~n", [binary_to_list(EOError)])
-	    end,
-		    
+      EORes = port_control(Port, ?GET_ENCRYPTED_OUTPUT, Data),
+      ?PRINT("GET_ENCRYPTED_OUTPUT: ~p~n", [EORes]),
+      case EORes of
+    <<0, Out/binary>> ->
+        gen_tcp:send(Socket, Out);
+    <<1, EOError/binary>> ->
+        ?PRINT("GET_ENCRYPTED_OUTPUT error: ~p~n", [binary_to_list(EOError)])
+      end,
+        
 
-	    loop(Port, Socket);
-	Msg ->
-	    ?PRINT("receive: ~p~n", [Msg]),
-	    loop(Port, Socket)
+      loop(Port, Socket);
+  Msg ->
+      ?PRINT("receive: ~p~n", [Msg]),
+      loop(Port, Socket)
     end.
 
 
@@ -321,8 +262,8 @@ get_cert_verify_string(CertVerifyRes, Cert) ->
     {CertMod, CertFun} = ?CERT_SELFSIGNED,
     IsSelfsigned = apply(CertMod, CertFun, [BCert]),
     case {CertVerifyRes, IsSelfsigned} of
-	{21, true} -> <<"self-signed certificate">>;
-	_ -> cert_verify_code(CertVerifyRes)
+  {21, true} -> <<"self-signed certificate">>;
+  _ -> cert_verify_code(CertVerifyRes)
     end.
 
 %% http://www.openssl.org/docs/apps/verify.html
