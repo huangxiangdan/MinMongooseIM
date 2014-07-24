1af1c4738b8d614afbe051aaacc8470ae13b5130
huangxiangdan <xd_huang1986@163.com>
2014-07-24 16:54:05 +0800

    change ejabberd_offline_prowl to genserver

--- apps/ejabberd/src/mod_offline_prowl.erl
+++ apps/ejabberd/src/mod_offline_prowl.erl
@@ -29,11 +29,17 @@
 -author('rgeorge@midnightweb.net').
 
 -behaviour(gen_mod).
+-behaviour(gen_server).
 
 -export([start/2,
-	 init/2,
 	 stop/1,
 	 send_notice/3]).
+
+% supervisor
+-export([start_link/2]).
+
+% gen_server
+-export([code_change/3,handle_call/3,handle_cast/2,handle_info/2,init/1,terminate/2]).
 
 -define(PROCNAME, ejabberd_offline_prowl).
 
@@ -41,70 +47,130 @@
 -include("logger.hrl").
 -include("jlib.hrl").
 
-start(Host, Opts) ->
-    ?INFO_MSG("Starting mod_offline_prowl", [] ),
-    register(gen_mod:get_module_proc(Host, ?PROCNAME),spawn(?MODULE, init, [Host, Opts])),
-    ok.
+-record(state, {vhost, address, apikey}).
 
-init(Host, _Opts) ->
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ejabberd starts module
+start(VHost, Opts) ->
+    ?INFO_MSG("Starting mod_offline", [] ),
+    ChildSpec =
+        {gen_mod:get_module_proc(VHost, ?PROCNAME),
+         {?MODULE, start_link, [VHost, Opts]},
+         permanent,
+         1000,
+         worker,
+         [?MODULE]},
+    % add child to ejabberd_sup
+    supervisor:start_child(ejabberd_sup, ChildSpec).
+
+% supervisor starts gen_server
+start_link(VHost, Opts) ->
+    Proc = gen_mod:get_module_proc(VHost, ?PROCNAME),
+    {ok, Pid} = gen_server:start_link({local, Proc}, ?MODULE, [VHost, Opts], []),
+    Pid ! start,
+    {ok, Pid}.
+
+init([VHost, _Opts]) ->
+    ?INFO_MSG("init mod_offline_prowl Host ~s", [VHost] ),
+    process_flag(trap_exit, true),
     inets:start(),
     ssl:start(),
-    ?INFO_MSG("init Host ~s", [Host] ),
-    ejabberd_hooks:add(offline_message_hook, Host, ?MODULE, send_notice, 10),
+    ejabberd_hooks:add(offline_message_hook, VHost, ?MODULE, send_notice, 10),
+    APIKeys = gen_mod:get_module_opt(VHost, ?MODULE, apikeys, fun(A) -> A end, [] ),
+    Addresses = gen_mod:get_module_opt(VHost, ?MODULE, addresses, fun(A) -> A end, [] ),
+    AddressArray = lists:keyfind(VHost, 1, Addresses),
+    ?INFO_MSG("AddressArray ~p", [AddressArray] ),
+    Address = element(2,AddressArray),
+    APIKey = lists:keyfind(VHost, 1, APIKeys),
+    APIToken = element(2,APIKey),
+    ?INFO_MSG("Found API Key for ~s. Will post message to ~s.~n", [element(1,APIKey), Address] ),
+
+    {ok, #state{vhost=VHost,
+                address=Address,
+                apikey=APIToken
+                }}.
+
+stop(VHost) ->
+    ?INFO_MSG("Stopping mod_remote_log", [] ),
+    Proc = gen_mod:get_module_proc(VHost, ?PROCNAME),
+    %gen_server:call(Proc, {cleanup}),
+    %timer:sleep(10000),
+    ok = supervisor:terminate_child(ejabberd_sup, Proc),
+    ok = supervisor:delete_child(ejabberd_sup, Proc).
+
+%%====================================================================
+%% Hooks
+%%====================================================================
+
+
+send_notice(From, To, P) -> 
+    ?INFO_MSG("aaa ~p", [{From, To}]),
+    VHost = From#jid.lserver,
+    Proc = gen_mod:get_module_proc(VHost, ?PROCNAME),
+    gen_server:cast(Proc, {addlog, From, To, P}).
+
+handle_call(Msg, _From, State) ->
+    ?INFO_MSG("Got call Msg: ~p, State: ~p", [Msg, State]),
+    {noreply, State}.
+
+% ejabberd_hooks call
+handle_cast({addlog, From, To, Packet}, #state{apikey=APIKey, address=Address} = State) ->
+    check_and_forward(From, To, Packet, APIKey, Address),
+    {noreply, State};
+
+handle_cast(Msg, State) ->
+    ?INFO_MSG("Got cast Msg:~p, State:~p", [Msg, State]),
+    {noreply, State}.
+
+handle_info(Info, State) ->
+    ?INFO_MSG("Got Info:~p, State:~p", [Info, State]),
+    {noreply, State}.
+
+terminate(Reason, State) ->
+    ?INFO_MSG("Reason: ~p", [Reason]),
+    cleanup(State),
     ok.
 
-stop(Host) ->
-    ?INFO_MSG("Stopping mod_offline_prowl", [] ),
-    ejabberd_hooks:delete(offline_message_hook, Host,
-			  ?MODULE, send_notice, 10),
-    ok.
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
 
-send_notice(_From, To, Packet) ->
-    Type = xml:get_tag_attr_s(<<"type">>, Packet),
-    % FromS = xml:get_tag_attr_s("from", Packet),
+cleanup(#state{vhost=VHost} = _State) ->
+  ?INFO_MSG("Stopping ~s for ~p", [?MODULE, VHost]),
+  ejabberd_hooks:delete(user_send_packet, VHost,
+        ?MODULE, send_notice, 10).
+
+% verifier si le trafic est local
+% Modified from original version: 
+%    - registered to the user_send_packet hook, to be called only once even for multicast
+%    - do not support "private" message mode, and do not modify the original packet in any way
+%    - we also replicate "read" notifications
+check_and_forward(_From, _To, #xmlel{attrs = Attrs} = Packet, APIKey, Address)->
+    Type = xml:get_attr_s(<<"type">>, Attrs),
     FromID = element(2, _From),
     FromServer = element(3, _From),
     From = [FromID, "@", FromServer],
     Body = xml:get_path_s(Packet, [{elem, <<"body">>}, cdata]),
-    % ?INFO_MSG("Packet ~p, Type ~p, Body ~p", [Packet, Type, Body] ),
     if
-	(Type /= <<"error">>) and (Type /= <<"groupchat">>)
-   and (Type /= <<"headline">>) and (Body /= <<"">>) ->
-    ToSB   = xml:get_tag_attr_s(<<"to">>, Packet),
-    ToS = binary_to_list(ToSB),
-    case ToS of
-      [] ->
-        ok;
-      _ ->
-        ToJID = string:sub_word(ToS,1,$/),
-        ToServer = string:sub_word(ToJID,2,$@),
-        APIKeys = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, apikeys, fun(A) -> A end, [] ),
-        Addresses = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, addresses, fun(A) -> A end, [] ),
-        AddressArray = lists:keyfind(list_to_binary(ToServer), 1, Addresses),
-        ?INFO_MSG("AddressArray ~p", [AddressArray] ),
-        case AddressArray of
-          false ->
-            ok;
-          _ ->
-            Address = element(2,AddressArray),
-            APIKey = lists:keyfind(list_to_binary(ToServer), 1, APIKeys),
-            ?INFO_MSG("Found API Key for ~s. Will post message to ~s.~n", [element(1,APIKey), Address] ),
-            Sep = "&",
-            Post = [
-              "api_token=", element(2,APIKey), Sep,
-              % "application=XMPP", Sep,
-              % "event=New%20Chat", Sep,
-              "message=", Body, Sep,
-              "from=", From, Sep,
-              % "priority=-1", Sep,
-              "to=", string:sub_word(ToS,1,$/) ],
-            Content = list_to_binary(Post),
-
-            httpc:request(post, {binary_to_list(Address), [], "application/x-www-form-urlencoded", Content},[],[]),
-            ok
-        end
-    end;
-  true ->
-    ok
+    (Type /= <<"error">>) and (Type /= <<"groupchat">>)
+ and (Type /= <<"headline">>) and (Body /= <<"">>) ->
+      ToSB   = xml:get_tag_attr_s(<<"to">>, Packet),
+      ToS = binary_to_list(ToSB),
+      case ToS of
+        [] ->
+          ok;
+        _ ->
+          Sep = "&",
+          Post = [
+            "api_token=", APIKey, Sep,
+            "message=", Body, Sep,
+            "from=", From, Sep,
+            "to=", string:sub_word(ToS,1,$/)],
+          ?INFO_MSG("Found API Key for ~s. Will post message to ~s, Body is ~s.~n", [APIKey, Address, Post] ),
+          Content = list_to_binary(Post),
+          httpc:request(post, {binary_to_list(Address), [], "application/x-www-form-urlencoded", Content},[],[]),
+          ok
+      end;
+    true ->
+      ok
     end.
-
